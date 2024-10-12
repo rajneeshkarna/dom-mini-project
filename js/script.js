94bbbff1a34d759f6ccf6fde4c55fe536293eef2
Rajneesh Kumar Karna <rkkarna123@gmail.com>
2024-10-13 03:07:00 +0530

    add style to wrapper div

---
+++ js/script.js
@@ -0,0 +1,2 @@
+const wrapper = document.querySelector('.wrapper');
+wrapper.style= 'display: flex; flex-direction: column; width: 60%; height: 60vh; background: green; color: white; margin: 60px auto';
