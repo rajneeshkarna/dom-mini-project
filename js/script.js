6b40fb0eb7f6fa2604b5b0ef5b52293f44a2cb4f
Rajneesh Kumar Karna <rkkarna123@gmail.com>
2024-10-13 13:51:23 +0530

    add span to h1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 const wrapper = document.querySelector('.wrapper');
 wrapper.style= 'display: flex; flex-direction: column; width: 60%; height: 60vh; background: green; color: white; margin: 60px auto';
+const firstHeading = document.querySelector('h1');
+firstHeading.innerHTML = firstHeading.innerHTML.replace('2020', '<span id = "year">2020</span>');
+const year = document.getElementById('year');
+year.style = 'color: yellow; background: black; padding: 3px 9px; border-radius: 3px';
