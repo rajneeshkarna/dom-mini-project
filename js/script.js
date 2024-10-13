31f8aa648386a3915e537d83b78def556b0d8cb4
Rajneesh Kumar Karna <rkkarna123@gmail.com>
2024-10-13 15:06:21 +0530

    add changeColor and setInterval to every 500ms

--- js/script.js
+++ js/script.js
@@ -3,4 +3,15 @@ wrapper.style= 'display: flex; flex-direction: column; width: 60%; height: 60vh;
 const firstHeading = document.querySelector('h1');
 firstHeading.innerHTML = firstHeading.innerHTML.replace('2020', '<span id = "year">2020</span>');
 const year = document.getElementById('year');
-year.style = 'color: yellow; background: black; padding: 3px 9px; border-radius: 3px';
+// year.style = `color: ${changeColor()}; background: gray; padding: 3px 9px; border-radius: 3px`;
+function changeColor(){
+  let arr = [];
+  for(let i = 0; i < 3; i++){
+    let random = Math.floor(Math.random() * 256);
+    arr.push(random);
+}
+let color = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
+return year.style = `color: ${color}; padding: 3px 9px; border-radius: 3px; font-size: 42px`; 
+}
+
+setInterval(changeColor, 500);
