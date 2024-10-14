6c8bde206c8e55a456e61f223227810516ffcb50
Rajneesh Kumar Karna <rkkarna123@gmail.com>
2024-10-14 23:44:51 +0530

    add date-time and style using dom

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const wrapper = document.querySelector('.wrapper');
-wrapper.style= 'display: flex; flex-direction: column; width: 60%; height: 60vh; background: green; color: white; margin: 60px auto';
+wrapper.style= 'display: flex; flex-direction: column; width: 60%; height: 60vh; background-color: rgb(63, 201, 192); color: black; margin: 60px auto; padding: 0px 36px';
 const firstHeading = document.querySelector('h1');
 firstHeading.innerHTML = firstHeading.innerHTML.replace('2020', '<span id = "year">2020</span>');
 const year = document.getElementById('year');
@@ -14,4 +14,22 @@ let color = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
 return year.style = `color: ${color}; padding: 3px 9px; border-radius: 3px; font-size: 42px`; 
 }
 
-setInterval(changeColor, 500);
+setInterval(changeColor, 1000);
+
+const fullDateTime = new Date();
+const newDate = document.createElement('p');
+newDate.innerText = `${fullDateTime}`;
+newDate.id = 'date';
+wrapper.appendChild(newDate);
+
+function changeColorDate(){
+  let arr = [];
+  for(let i = 0; i < 3; i++){
+    let random = Math.floor(Math.random() * 256);
+    arr.push(random);
+}
+let color = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
+return newDate.style = `background: ${color}; padding: 3px 9px; border-radius: 3px; font-size: 18px; color: white; margin-top: 12px`; 
+}
+
+setInterval(changeColorDate, 1000);
